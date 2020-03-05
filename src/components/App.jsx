28b6312ef535721cdeef87bd39ff892f3ba6fb8e
Trevor Mackin <tismetrm@gmail.com>
2020-03-05 13:05:59 -0800

    Add background image with scroll overflow to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,10 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Error404 from './Error404';
 import Home from './Home';
+import Modal from './Modal';
+import Nav from './Nav';
+import bgImage from '../assets/images/bg1.jpg';
 // import AllProducts from './AllProducts';
-import ProductList from './ProductList';
 // import c from './../constants';
 // import Moment from 'moment';
 // import { v4 } from 'uuid';
@@ -14,32 +16,53 @@ import ProductList from './ProductList';
 
 function App() {
 
+
   return (
-    <div>
+    <div style={background}>
+      <Nav />
       <Switch>
         <Route exact path='/' render={() => <Home />} />
 
-        <Route path='/productlist' render={() => <ProductList
-          listOfTickets={this.props.masterTicketList} />} />
+        <Route path='/modal' render={() => <Modal />} />
+
+        {/* <Route path='/productlist' render={() => <ProductList
+          listOfTickets={this.props.masterTicketList} />} /> */}
 
         <Route component={Error404} />
       </Switch>
-      <style global jsx>{`
-  
-        `}</style>
+      <style global jsx>{` `}</style> 
     </div>
   );
 }
 
 
 App.propTypes = {
-  masterTicketList: PropTypes.object
+  masterProductList: PropTypes.array
 };
+
 
 const mapStateToProps = state => {
   return {
-    masterTicketList: state.masterTicketList
+    masterProductList: state.masterProductList
   };
 };
 
+
 export default withRouter(connect(mapStateToProps)(App));
+
+
+
+
+
+/* ------------------ CSS Styling Below ------------------ */
+
+var background = {
+  backgroundImage: `url(${bgImage})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '100% 100%',
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+  overflow: 'scroll',
+  overflowX: 'hidden'
+};
