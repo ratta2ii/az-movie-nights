8f6dccabc541f34bd3e41be8e18ccc011c41bf6b
Trevor Mackin <tismetrm@gmail.com>
2020-03-09 10:00:19 -0700

    Add WishList component to project Create and import a stylesheet into the WishList component

---
+++ src/components/WishList/WishList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './WishList.css';
+
+function WishList() {
+
+  return (
+    <div id={styles.wishListComponent}>
+      <h1>I am the WishList Component!</h1>
+    </div>
+  );
+}
+
+export default WishList;
