0843aba096e49410b1a4b527ff9d4764d50129d2
Trevor Mackin <tismetrm@gmail.com>
2020-03-09 18:41:41 -0700

    Add a wish-list reducer that will add a product to list

--- src/reducers/wish-list-reducer.js
+++ src/reducers/wish-list-reducer.js
@@ -1,19 +1,61 @@
-export default (state = {} , action) => {
+/* eslint-disable indent */
+export default (state = {}, action) => {
   let newState;
   switch (action.type) {
-  case 'ADD_PRODUCT':
-    let currentState = state;
-    newState = Object.assign({}, currentState, action.product);
-    console.log('I am the new state');
-    console.log(newState);
-    return newState;
-  default:
-    return state;
+    case 'ADD_PRODUCT':
+      newState = Object.assign({}, state, {
+        [action.product.productId]: {
+          title: action.product.title,
+          price: action.product.price
+        }
+      });
+      return newState;
+    default:
+      return state;
   }
 };
 
 
-// console.log('I am the state');
-// console.log(state[0]);
-// console.log('I am the action');
-// console.log(action.product);
+
+// export default (state = {}, action) => {
+
+//   console.log('I am the action');
+//   console.log(action.product);
+//   let newState;
+//   switch (action.type) {
+//   case 'ADD_PRODUCT':
+//     newState = Object.assign({}, state, action.product);
+//     console.log('I am the new state');
+//     console.log(newState);
+//     return newState;
+//  
+//   }
+// };
+
+
+
+
+// export default (state = {}, action) => {
+//   let newState;
+//   const { productId, price, title } = action;
+
+//   switch (action.type) {
+//     case 'ADD_TICKET':
+//       newState = Object.assign({}, state, {
+//         [productId]: {
+//           price: price,
+//           title: title
+//         }
+//       });
+//       console.log('I am the new state');
+//       console.log(newState);
+//       console.log('I am the action');
+//       console.log(action);
+
+//       return newState;
+//     default:
+//       return state;
+//   }
+// };
+
+
